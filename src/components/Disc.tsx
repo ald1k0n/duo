ac1c3a60d2768475522269e4782502fb7e7b492b
AbylaiNur <abylai.nurskenev@gmail.com>
2024-04-13 20:25:03 +0500

    feat: changed css of Disk.tsx

--- src/components/Disc.tsx
+++ src/components/Disc.tsx
@@ -1,24 +1,26 @@
-import { Card } from 'antd';
-import { FC } from 'react';
+import { Card } from "antd";
+import { FC } from "react";
 
 type IProps = {
-	title?: string;
-	isDone?: boolean;
+  title?: string;
+  isDone?: boolean;
 };
 
 export const Disc: FC<IProps> = ({ title, isDone }) => {
-	return (
-		<div className='relative'>
-			<Card
-				className={`bg-gray-600 rounded-full w-[69px] h-[65px] md:w-[140px] md:h-[112px] shadow-xl flex justify-center items-center`}
-				style={{ zIndex: 1 }}></Card>
-			<Card
-				className={`${
-					!isDone ? 'bg-[#D9D9D9]' : 'bg-red-700'
-				} rounded-full w-[69px] h-[65px] md:w-[140px] md:h-[112px] hover:bg-gray-300 shadow-xl flex justify-center items-center absolute top-[-8px] left-0 right-0 mx-auto`}
-				style={{ zIndex: 2 }}>
-				{title}
-			</Card>
-		</div>
-	);
+  return (
+    <div>
+      <Card
+        className={`${
+          !isDone ? "bg-[#D9D9D9]" : "bg-red-700"
+        } rounded-full w-[69px] h-[69px] md:w-[140px] md:h-[140px] hover:bg-gray-300 shadow-xl flex justify-center items-center  top-[-8px] left-0 right-0 mx-auto`}
+        style={{
+          zIndex: 2,
+          transform: "rotateX(0.15turn)",
+          boxShadow: "0 13px 4px 0 rgba(0,0,0,.25)",
+        }}
+      >
+        {title}
+      </Card>
+    </div>
+  );
 };
