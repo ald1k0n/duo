84f8cae7fb5294bfab04e4528795689d0b240269
AbylaiNur <abylai.nurskenev@gmail.com>
2024-04-13 21:15:46 +0500

    feat: fix 1

--- src/pages/Module/Grammar.tsx
+++ src/pages/Module/Grammar.tsx
@@ -1,16 +1,19 @@
-<<<<<<< HEAD
 import { Disc } from "@/components";
 import { ModuleService } from "@/services";
 import { useAuthStore } from "@/shared/stores/useAuthStore";
+import { useModulesStore } from "@/shared/stores/useModulesStore";
 import { Flex, Layout, notification } from "antd";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Grammar() {
   const service = new ModuleService();
   const [lessons, setLessons] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const { setCurrentLesson } = useModulesStore();
   const { user } = useAuthStore();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     (async () => {
@@ -34,7 +37,7 @@ export default function Grammar() {
   if (isLoading) {
     return <div>Loading...</div>;
   }
-console.log(lessons);
+  console.log(lessons);
 
   return (
     <Layout
@@ -48,76 +51,18 @@ console.log(lessons);
           <Flex
             key={index}
             wrap="wrap"
+            onClick={() => {
+              setCurrentLesson(lesson);
+              navigate(`./${lesson.title}`);
+            }}
             justify={index === 0 ? "center" : index % 2 === 1 ? "end" : "start"}
             gap={16}
             style={{ marginBottom: 16 }}
           >
-            <Disc title={lesson.title}  />
+            <Disc title={lesson.title} />
           </Flex>
         ))}
       </div>
     </Layout>
   );
-=======
-import { Disc } from '@/components';
-import { ModuleService } from '@/services';
-import { useModulesStore } from '@/shared/stores/useModulesStore';
-import { Flex, Layout, notification } from 'antd';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-export default function Grammar() {
-	const service = new ModuleService();
-	const [lessons, setLessons] = useState<any[]>([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const navigate = useNavigate();
-	const { setCurrentLesson } = useModulesStore();
-
-	useEffect(() => {
-		(async () => {
-			try {
-				const data = await service.getAllModules();
-				// setData(data[1].lessons);
-				setLessons(data[1].lessons as unknown[]);
-				setIsLoading(false);
-			} catch (error) {
-				setIsLoading(false);
-				notification.error({
-					message: 'Error Happened',
-					duration: 5,
-					placement: 'topRight',
-				});
-			}
-		})();
-	}, []);
-
-	if (isLoading) {
-		return <div>Loading...</div>;
-	}
-
-	return (
-		<Layout
-			className='mt-4'
-			style={{
-				backgroundColor: '#A3C644',
-			}}>
-			<div className='w-full md:w-[400px] mx-auto px-3 mt-2'>
-				{lessons?.map((lesson, index) => (
-					<Flex
-						key={index}
-						wrap='wrap'
-						justify={index === 0 ? 'center' : index % 2 === 1 ? 'end' : 'start'}
-						gap={16}
-						onClick={() => {
-							setCurrentLesson(lesson);
-							navigate(`./${lesson.title}`);
-						}}
-						style={{ marginBottom: 16 }}>
-						<Disc title={lesson.title} />
-					</Flex>
-				))}
-			</div>
-		</Layout>
-	);
->>>>>>> 7c8814155294d8e462a68d030ea3601bda3d08db
 }
