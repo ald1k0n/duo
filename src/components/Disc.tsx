be608a1488a3d43e948dc0d624daa515a0105a7e
AbylaiNur <abylai.nurskenev@gmail.com>
2024-04-14 09:58:58 +0500

    changed checkers

--- src/components/Disc.tsx
+++ src/components/Disc.tsx
@@ -1,38 +1,45 @@
-import { Card } from 'antd';
-import { FC } from 'react';
-import { CheckOutlined } from '@ant-design/icons';
+import { Card } from "antd";
+import { FC } from "react";
+import { CheckOutlined } from "@ant-design/icons";
 
 type IProps = {
-	title?: string;
-	isDone?: boolean;
+  title?: string;
+  isDone?: boolean;
 };
 
 export const Disc: FC<IProps> = ({ title, isDone }) => {
-	return (
-		<div>
-			<Card
-				className={`${
-					isDone ? 'bg-[#39c2d7]' : 'white'
-				} w-[125px] h-[80px] md:w-[140px] md:h-[90px] hover:bg-gray-300 text-center shadow-xl flex justify-center items-center  top-[-8px] left-0 right-0 mx-auto`}
-				style={{
-					zIndex: 2,
-					border: 'none',
-					boxShadow: '0 8px 4px 0 rgba(0,0,0,.25)',
-					borderBottom: 'none',
-					borderRadius: '50%',
-				}}>
-				{isDone ? (
-					<CheckOutlined
-						style={{
-							transform: 'rotateX(40deg)',
-							fontSize: '60px',
-							color: 'white',
-						}}
-					/>
-				) : (
-					title
-				)}
-			</Card>
-		</div>
-	);
+  return (
+    <div>
+      <Card
+        className={`${
+          isDone ? "bg-[#39c2d7]" : "bg-[#d9d9d9]"
+        } w-[125px] h-[80px] md:w-[140px] md:h-[90px] hover:bg-gray-300 text-center shadow-xl flex justify-center items-center  top-[-8px] left-0 right-0 mx-auto`}
+        style={{
+          zIndex: 2,
+          border: "none",
+          boxShadow: "0 8px 4px 0 rgba(0,0,0,.25)",
+          borderBottom: "none",
+          borderRadius: "50%",
+        }}
+      >
+        {isDone ? (
+          <CheckOutlined
+            style={{
+              transform: "rotateX(40deg)",
+              fontSize: "60px",
+              color: "white",
+            }}
+          />
+        ) : (
+          <CheckOutlined
+            style={{
+              transform: "rotateX(40deg)",
+              fontSize: "60px",
+              color: "#B1B1B1",
+            }}
+          />
+        )}
+      </Card>
+    </div>
+  );
 };
