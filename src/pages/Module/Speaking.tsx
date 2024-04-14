fe8166d77e15e896cd9d377b7731a0e29e4ac262
AbylaiNur <abylai.nurskenev@gmail.com>
2024-04-14 09:51:11 +0500

    commit

--- src/pages/Module/Speaking.tsx
+++ src/pages/Module/Speaking.tsx
@@ -1,119 +1,142 @@
-import { Disc } from '@/components';
-import { ModuleService } from '@/services';
-import { useAuthStore } from '@/shared/stores/useAuthStore';
-import { useModulesStore } from '@/shared/stores/useModulesStore';
-import { Flex, Layout, notification, Typography } from 'antd';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Lesson } from '@/types.ts';
+import { Disc } from "@/components";
+import { ModuleService } from "@/services";
+import { useAuthStore } from "@/shared/stores/useAuthStore";
+import { useModulesStore } from "@/shared/stores/useModulesStore";
+import { Button, Flex, Layout, notification, Popover, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Lesson } from "@/types.ts";
 
 export default function Speaking() {
-	const service = new ModuleService();
-	const [lessons, setLessons] = useState<Lesson[]>([]);
+  const service = new ModuleService();
+  const [lessons, setLessons] = useState<Lesson[]>([]);
 
-	const [isLoading, setIsLoading] = useState(true);
-	const { setCurrentLesson } = useModulesStore();
-	const { user } = useAuthStore();
+  const [isLoading, setIsLoading] = useState(true);
+  const { setCurrentLesson } = useModulesStore();
+  const { user } = useAuthStore();
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		(async () => {
-			try {
-				const data = await service.getAllModules();
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await service.getAllModules();
 
-				const lessonData = data?.map((d) => {
-					const lessonsWithModuleId = d.lessons.map((lesson, idx) => ({
-						...lesson,
-						moduleId: d.id,
-						idx,
-					}));
-					return lessonsWithModuleId;
-				});
+        const lessonData = data?.map((d) => {
+          const lessonsWithModuleId = d.lessons.map((lesson, idx) => ({
+            ...lesson,
+            moduleId: d.id,
+            idx,
+          }));
+          return lessonsWithModuleId;
+        });
 
-				if (lessonData) {
-					setLessons(lessonData.flat());
-				}
+        if (lessonData) {
+          setLessons(lessonData.flat());
+        }
 
-				setIsLoading(false);
-			} catch (error) {
-				setIsLoading(false);
-				notification.error({
-					message: 'Error Happened',
-					duration: 5,
-					placement: 'topRight',
-				});
-			}
-		})();
-	}, []);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        notification.error({
+          message: "Error Happened",
+          duration: 5,
+          placement: "topRight",
+        });
+      }
+    })();
+  }, []);
 
-	if (isLoading) {
-		return <div>Loading...</div>;
-	}
-	return (
-		<Layout
-			className='pt-4'
-			style={{
-				backgroundColor: '#A3C644',
-				minHeight: 'calc(100vh + 30vh)',
-			}}>
-			<Typography.Title
-				className='text-center'
-				level={3}
-				style={{
-					color: 'white',
-				}}>
-				1-ші деңгей
-			</Typography.Title>
-			<div className='w-full md:w-[400px] mx-auto px-3 mt-2'>
-				{lessons?.map((lesson, index) => (
-					<Flex
-						key={index}
-						wrap='wrap'
-						onClick={() => {
-							if (!user) {
-								notification.info({
-									message: 'You must be authorized to start the lessson',
-									duration: 5,
-									placement: 'topRight',
-								});
-							} else {
-								setCurrentLesson(lesson);
-								navigate(`./${lesson.title}`, {
-									state: {
-										moduleData: {
-											moduleId: lesson.moduleId,
-										},
-										lessonIdx: lesson.idx,
-										isAudio: true,
-									},
-								});
-							}
-						}}
-						justify={'center'}
-						gap={16}
-						style={{ marginBottom: 16 }}>
-						<div
-							style={{
-								transform: `translateX(${calculateTransitionPx(index)}px)`,
-							}}>
-							<Disc
-								title={lesson.title}
-								isDone={
-									!!user &&
-									!!user.id &&
-									lesson.passedStudentIds?.includes(user.id)
-								}
-							/>
-						</div>
-					</Flex>
-				))}
-			</div>
-		</Layout>
-	);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Layout
+      className="mt-4"
+      style={{
+        backgroundColor: "#A3C644",
+        minHeight: "calc(100vh + 30vh)",
+      }}
+    >
+      <Typography.Title
+        className="text-center"
+        level={3}
+        style={{
+          color: "white",
+        }}
+      >
+        1-ші деңгей
+      </Typography.Title>
+      <div className="w-full md:w-[400px] mx-auto px-3 mt-2">
+        {lessons?.map((lesson, index) => (
+          <Flex
+            key={index}
+            wrap="wrap"
+            justify={"center"}
+            gap={16}
+            style={{ marginBottom: 16 }}
+          >
+            <div
+              style={{
+                transform: `translateX(${calculateTransitionPx(index)}px)`,
+              }}
+            >
+              <Popover
+                placement="bottom"
+                title={lesson.title}
+                overlayInnerStyle={{
+                  backgroundColor: "#39C2D7",
+                }}
+                content={
+                  <div>
+                    <Button
+                      onClick={() => {
+                        if (!user) {
+                          notification.info({
+                            message:
+                              "You must be authorized to start the lessson",
+                            duration: 5,
+                            placement: "topRight",
+                          });
+                        } else {
+                          setCurrentLesson(lesson);
+                          navigate(`./${lesson.title}`, {
+                            state: {
+                              moduleData: {
+                                moduleId: lesson.moduleId,
+                              },
+                              lessonIdx: lesson.idx,
+                              isAudio: true,
+                            },
+                          });
+                        }
+                      }}
+                    >
+                      Бастау!
+                    </Button>
+                  </div>
+                }
+                trigger="click"
+              >
+                <Button type="link" style={{ position: "absolute" }}></Button>
+                <Disc
+                  title={lesson.title}
+                  isDone={
+                    !!user &&
+                    !!user.id &&
+                    lesson.passedStudentIds?.includes(user.id)
+                  }
+                />
+              </Popover>
+            </div>
+          </Flex>
+        ))}
+      </div>
+    </Layout>
+  );
 }
 
 function calculateTransitionPx(index: number) {
-	const radius = 100;
-	return radius * Math.sin((index * Math.PI) / 3.2);
+  const radius = 100;
+  return radius * Math.sin((index * Math.PI) / 3.2);
 }
